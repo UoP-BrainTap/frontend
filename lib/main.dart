270d0d5402a71e1f4470cb7a5abc8d0a4402845d
Benjamin Lovering <benjaminlovering1@gmail.com>
2025-02-25 00:02:52 +0000

    Added dio with access token interceptor

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:frontend/pages/home_page.dart';
 import 'package:frontend/pages/login_page.dart';
 import 'package:frontend/pages/signup_page.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_strategy/url_strategy.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   setPathUrlStrategy();
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    // ChangeNotifierProvider(create: (context) => AuthProvider()),
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -34,15 +39,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        fontFamily: 'Poppins',
-        brightness: Brightness.dark,
-        inputDecorationTheme: const InputDecorationTheme(
-          border: OutlineInputBorder(),
+    return Provider<Dio>(
+      create: (_) => DioProvider().dio,
+      child: MaterialApp.router(
+        theme: ThemeData(
+          fontFamily: 'Poppins',
+          brightness: Brightness.dark,
+          inputDecorationTheme: const InputDecorationTheme(
+            border: OutlineInputBorder(),
+          ),
         ),
+        routerConfig: _router,
       ),
-      routerConfig: _router,
     );
+  }
+}
+
+class DioProvider {
+  Dio get dio {
+    var instance = Dio();
+    instance.options.baseUrl = 'http://localhost:8080';
+    instance.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) async {
+          // get access token from shared preferences
+          var shared = await SharedPreferences.getInstance();
+          var token = shared.getString('accessToken');
+          if (token == null || token.isEmpty) {
+            handler.next(options);
+            return;
+          }
+
+          // add token to headers
+          options.headers['Authorization'] = 'Bearer $token';
+          handler.next(options);
+        }
+      )
+    );
+    return instance;
   }
 }
